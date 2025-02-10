d339accd17521fa568c643e5aa332ae32cfa3f94
Josh <j.fogg@ed.ac.uk>
2025-02-10 15:50:19 +0000

    Added Jacob's original code and explanation

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# sort_homework.jl
-A script for distributing homework submissions to tutors for marking.
+# `sort_homework.jl`
+
+A script for distributing homework submissions to tutors for marking, used in the University of Edinburgh's [School of Economics](https://economics.ed.ac.uk/).
+
+## Installation
+
+Use the windows store to install both [julia](https://julialang.org/) and [VS Code](https://code.visualstudio.com/).  We've checked, and it looks like both of these can be installed on your desktops without any additional administrator privileges.
+
+Once you're in VS Code, you should go to the "Extensions" tab (on the left hand side) to search for and install the julia extension.
+
+## Running the Script
+
+Make sure that the files sort_homework.jl and Project.toml are both in the same folder as the directory containing the student submissions downloaded from Learn.
+
+In VS Code, open the folder for the current week's homework
+
+Open the sort_homework.jl file and update the configuration section at the top.  You may need to change the names of the:
+
+- marksheet: this is the only one that will typically need to be changed week to week.  Copy over the filename of the current week's marksheet.
+
+- tutors: You will need to copy over the names of the tutors exactly as they appear in the marksheet.  This should only need to be done once a semester, or when the tutors change.
+
+- file_dir: If you want to change the name of the folder where the submissions are located, you may need to update this.  As of now, it's set to the default that Learn produces when you unzip the files ("Submissions/")
+
+- out_dir: If you want to change the name of the output directory where the sorted homeworks will be stored, change this to the desired name.
+
+Open a terminal.  You can do this by typing Ctrl + Shift + P to open the command palette, and then typing **"Create New Terminal"**.  I think there's also a keyboard shortcut you can use to do this directly (Ctrl + Shift + `).  If you opened VS Code in the correct folder in step 2, then you shouldn't need to change anything further to ensure you're in the correct directory.
+
+Run the sorting file by typing the command `julia sort_homework.jl` in the terminal prompt.
+
+Note 1: You can also do this by opening the file in the text editor, and, using the command palette, execute the command "Julia: Execute File in REPL"
+
+Note 2: This will delete the folder sorted_homeworks and re-create it, so if you're running this command for the second time, you may want to create a backup copy of the sorted homeworks folder (if, for instance, the tutors have made annotations on the scripts).
+
+I've attached the script and the Project.toml file to this email again for reference.  Note that I'm pretty sure that this sorting script should work with any student submissions on Learn that need to be sorted by Tutors (as long as the marksheet has the same format) so you may be able to use this as well for things like sorting essay submissions.
---
+++ sort_homework.jl
@@ -0,0 +1,100 @@
+#= 
+Title:  sort_homework.jl
+Author: Jacob Adenbaum
+
+
+Description: This script will sort the homework files by tutor.  There are configurable options (mostly filenames/directory paths) in the top section labelled configuration.  
+
+Dependencies:
+    - marksheet: this script uses the current marksheet to read the
+    correspondence between student IDs and Tutor names.  You need to provide the
+    name of the excel file that contains the marksheet
+        NOTE: this script relies on details about how the marksheet is formatted (column names, sheet names, spacing of the header).  You should ensure that the formatting does not change from week to week, otherwise this might break
+
+    - file_dir: the directory storing the raw homework files downloaded from Learn 
+    
+    - tutors: I've hardcoded the list of valid tutors in the configuration setting
+        you should ensure that the names are up to date with what is in the marksheet (and that they are consistently capitalized from week to week)
+
+Outputs:
+    - out_dir: this folder contains subfolders with the submissions for
+        each tutor, and where the submissions have been named according to each
+        student's ID.  
+
+        Note: if a student has submitted multiple documents, they will be collected together in a folder together.  
+
+        Note: this script will clear the output directory before running, so if
+        you're running this script for a second time, and you have made edits or
+        annotations to files in the output directory, be sure to create a
+        backup.  
+=# 
+
+################################################################################
+#################### CONFIGURATION #############################################
+################################################################################
+file_dir    = "Submissions/"
+out_dir     = "sorted_homeworks/"
+marksheet   = "Marksheet.xlsx"
+
+# List of tutors to use (this helps deal with the case where there are
+# extraneous rows in the marksheet)
+tutors = [
+    "Tutor 1",
+    "Tutor 2",
+    "Tutor 3"
+] 
+
+################################################################################
+#################### CODE TO RUN ###############################################
+################################################################################
+
+## Setup 
+using Pkg
+Pkg.activate(@__DIR__)
+Pkg.instantiate()
+
+cd(@__DIR__)
+using CSV, DataFrames, DataFramesMeta, Chain
+using XLSX
+
+## Load the tutor list
+# df = CSV.File("tutor_list.csv") |> DataFrame 
+df = XLSX.readtable(marksheet, "Marksheet", first_row = 3) |> DataFrame
+
+df = @chain df begin
+   @rsubset!(:Tutor in tutors)
+   @rtransform!(:Username = lowercase(:Username))
+   select!(:Username, :Tutor)
+end
+
+files = readdir("Submissions")
+
+## Loop over all the students
+
+rm(out_dir, recursive=true, force=true)
+
+@eachrow df begin
+    # Filter just that student's submission files  
+    student_files = filter(
+        x -> occursin(:Username, x) & !occursin(r"\.txt$", x), 
+        files
+    )
+    
+    # Copy each file to the relevant folder 
+    for (i, file) in enumerate(student_files)
+        ext = match(r"\.\w*$", file).match
+        
+        # destination path
+        if length(student_files) == 1
+            dest = joinpath(out_dir, :Tutor, "$(:Username)$ext")
+        else
+            dest = joinpath(out_dir, :Tutor, :Username, "Sumbission$i$ext")
+        end
+        mkpath(dest)
+        
+        # Source path
+        src = joinpath(file_dir, file)
+
+        cp(src, dest, force=true)
+    end
+end
